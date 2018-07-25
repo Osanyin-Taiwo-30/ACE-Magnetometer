3cac4855cb69dda51f1d5f72abc17cd27ed550ca
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-07-24 22:22:49 -0400

    test soft download fail

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -4,6 +4,7 @@ import ace_magnetometer as am
 from pathlib import Path
 from datetime import date
 import pandas as pd
+import ftplib
 
 dt = '2013-02-12'
 R = Path(__file__).parent
@@ -11,7 +12,10 @@ R = Path(__file__).parent
 
 @pytest.fixture(scope="session", autouse=True)
 def test_download():
-    am.download(dt, R)
+    try:
+        am.download(dt, R)
+    except ftplib.error_temp:
+        pytest.skip('download down')
 
 
 def test_load():
